ca5ebf3e14800ff7bf402c0fdfd60a569e762779
acpennlab <147250307+acpennlab@users.noreply.github.com>
2024-03-11 23:34:35 +0000

    Delete startup.m

--- startup.m
+++
@@ -1,8 +0,0 @@
-# Add to octave-cli path in RStudio
-addpath ('/srv/conda/envs/notebook/share/octave/8.3.0/m/startup/', '-end')
-addpath ('/srv/conda/envs/notebook/share/octave/8.3.0/m/testfun/', '-end')
-addpath ('/srv/conda/envs/notebook/share/octave/8.3.0/m/set/', '-end')
-addpath ('/srv/conda/envs/notebook/lib/octave/8.3.0/oct/x86_64-conda-linux-gnu', '-end')
-addpath (genpath ('/srv/conda/envs/notebook/share/octave/site/m'), '-end')
-addpath (genpath ('/srv/conda/envs/notebook/share/octave/8.3.0/m'), '-end')
-addpath (genpath ('/srv/conda/envs/notebook/share/octave/8.3.0/data'), '-end')
